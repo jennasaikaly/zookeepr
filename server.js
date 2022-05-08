232915cec2d0be2a0c8217c48662308a8642612a
jennasaikaly <jen.saikaly@gmail.com>
2022-05-07 22:41:53 -0400

    added post requests

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { animals } = require('./data/animals'); 
 const PORT = process.env.PORT || 3001; //accessing heroku port
 const app = express();
+const fs = require('fs');
+const path = require('path');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -53,6 +55,37 @@ function filterByQuery(query, animalsArray) {
     return result;
   }
 
+//A function that accepts the POST route's req.body value and the array that we want to
+//add the data to.
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+  animalsArray.push(animal);
+  //adds animal to animalsArray
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+    // return finished code to post route for response
+    return animal;
+  }
+
+  //function to validate user input
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
   //ROUTES
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -73,8 +106,17 @@ app.get('/api/animals', (req, res) => {
 // POST ROUTES
 app.post('/api/animals', (req, res) => {
   // req.body is where our incoming content will be
-  console.log(req.body);
-  res.json(req.body);
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+  
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+  // add animal to json file and animals array in this function
+  const animal = createNewAnimal(req.body, animals);
+  res.json(animal);
+}
 });
 
   //links to PORT code at top of doc (Heroku's port)
